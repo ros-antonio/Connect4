361997b637328b9fae5207c799f17ffa80345fc5
ros-antonio <antonio.ros@stud.ubbcluj.ro>
2025-12-14 13:09:50 +0200

    modified unittest for domain

--- domain/test_domain.py
+++ domain/test_domain.py
@@ -8,29 +8,33 @@ class DomainTest(TestCase):
         self.__board = Board()
 
     def test_place_piece(self):
-        self.__board.place_piece(0, 'X')
-        new_board = [[' ' for _ in range(7)] for _ in range(6)]
-        new_board[5][0] = 'X'
+        self.__board.place_piece(0, 'red')
+
+        new_board = [[Board.EMPTY for _ in range(7)] for _ in range(6)]
+        new_board[5][0] = 'red'
+
         self.assertEqual(self.__board.get_board(), new_board)
 
     def test_place_piece_out_of_bounds(self):
         try:
-            self.__board.place_piece(7, 'X')
+            self.__board.place_piece(7, 'red')
             assert False
         except InvalidMove:
             assert True
 
     def test_place_piece_column_full(self):
         for _ in range(6):
-            self.__board.place_piece(0, 'X')
+            self.__board.place_piece(0, 'red')
+
         try:
-            self.__board.place_piece(0, 'X')
+            self.__board.place_piece(0, 'red')
             assert False
         except InvalidMove:
             assert True
 
     def test_remove_piece(self):
-        self.__board.place_piece(0, 'X')
+        self.__board.place_piece(0, 'red')
         self.__board.remove_piece(0)
-        new_board = [[' ' for _ in range(7)] for _ in range(6)]
+
+        new_board = [[Board.EMPTY for _ in range(7)] for _ in range(6)]
         self.assertEqual(self.__board.get_board(), new_board)
